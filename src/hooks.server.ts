3453a6f66f67a8df9ea7cd81c4703f1e2fdae03c
RbnMach <machaca.ruben@yahoo.com>
2023-09-22 18:10:00 -0400

    trabajando en la conexion

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,18 @@
-import { Pb } from '$lib/conexion-database/conexion_pocket';
+import PocketBase from 'pocketbase';
 export const handle = async ({ event, resolve }) => {
-	event.locals.pb = Pb(); 
+	event.locals.pb = new PocketBase('http://127.0.0.1:8090');
 	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 	try {
-		if (event.locals.pb.authStore.isValid) {
-			event.locals.user = structuredClone(await event.locals.pb.collection('cuenta').authRefresh());
-		} else {
-			event.locals.user = undefined;
-		}
+		// if (event.locals.pb.authStore.isValid) {
+		// event.locals.user = structuredClone(await event.locals.pb.collection('cuenta').authRefresh());
+		event.locals.pb.authStore.isValid && (await event.locals.pb.collection('users').authRefresh());
+		// } else {
+		// 	event.locals.user = undefined;
+		// }
 	} catch (_) {
 		event.locals.pb.authStore.clear();
 	}
-	
+
 	const response = await resolve(event);
 	const isProd = process.env.NODE_ENV === 'production' ? true : false;
 	response.headers.append(
--- src/lib/conexion-database/conexion_pocket.ts
+++ src/lib/conexion-database/conexion_pocket.ts
@@ -5,7 +5,7 @@ let pocket: PocketBase;
 let url = 'http://127.0.0.1:8090';
 export const Pb = () => {
 	if (!pocket) {
-		pocket = new PocketBase(url); 
+		pocket = new PocketBase('http://127.0.0.1:8090');
 	}
 	return pocket;
 };
