7344caa6493a15e4d828f66d0fcb2b5e6e896ad4
BenMach <machaca.ruben@yahoo.com>
2023-09-23 10:12:35 -0400

    __

--- src/lib/conexion-database/conexion_pocket.ts
+++
@@ -1,9 +0,0 @@
-import PocketBase from 'pocketbase';
-
-let pocket: PocketBase;
-export const Pb = () => {
-	if (!pocket) {
-		pocket = new PocketBase('http://127.0.0.1:8090');
-	}
-	return pocket;
-};
